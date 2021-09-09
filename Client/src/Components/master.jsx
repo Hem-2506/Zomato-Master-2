a2d9597210969ace33334c0a5b2535d7adbbb2a8
Hem-2506 <hemant.hs2506@gmail.com>
2021-09-10 00:24:44 +0700

    added nightlife andnutri

--- Client/src/Components/master.jsx
+++ Client/src/Components/master.jsx
@@ -4,12 +4,17 @@ import  {useParams} from "react-router-dom";
 // Components
 import Delivery from "./Delivery";
 import Dining from "./Dining";
+import NightLife from "./NightLife";
+import Nutrition from "./Nutrition";
+
 
 const Master = () => {
     const { type } = useParams();
     return <div className="my-5">
         {type === "delivery" && <Delivery />}
         {type === "dining" && <Dining />}
+        {type === "night" && <NightLife />}
+        {type === "nutri" && <Nutrition />}
         </div>;
    
 };
