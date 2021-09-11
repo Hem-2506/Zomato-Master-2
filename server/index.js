73ab114560054817583fbc6d7a1f8bebe44118bd
Hem-2506 <hemant.hs2506@gmail.com>
2021-09-11 12:31:48 +0530

    AWS Setup in backend

--- server/index.js
+++ server/index.js
@@ -15,8 +15,9 @@ import Auth from "./API/Auth";
 import Restaurant from "./API/Restuarant";
 import Food from "./API/Food";
 import Order from "./API/orders";
-import Reviews from "./API/reviews"
-import User from "./API/User"
+import Reviews from "./API/reviews";
+import User from "./API/User";
+import Image from "./API/Image";
 
 // Database Connection
 import ConnectDB from "./database/connection";
@@ -41,6 +42,7 @@ zomato.use("/food", Food);
 zomato.use("/order", Order);
 zomato.use("/reviews", Reviews);
 zomato.use("/user", User); 
+zomato.use("/image", Image); 
 
 zomato.get ("/", (req,res) => res.json ({messsage: "Setup success"}));
 
