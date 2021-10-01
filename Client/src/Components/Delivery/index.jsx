2fb02ffb45df13d93c66c6fe5f388ccc607de2b0
Hem-2506 <hemant.hs2506@gmail.com>
2021-10-02 00:24:42 +0530

    added upto img and rest reducer

--- Client/src/Components/Delivery/index.jsx
+++ Client/src/Components/Delivery/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 // Components
 import DeliveryCarousel from './DeliveryCarousel';
@@ -6,41 +7,15 @@ import Brand from './Brand';
 import RestaurantCard from '../RestaurantCard';
 
  const Delivery = () => {
-const [restaurantList] = useState([
-   {
-_id: "123456",
-photos:["https://b.zmtcdn.com/data/dish_photos/0a5/167b8a27b16d1656033f6f341917f0a5.jpg?output-format=webp"],
-cuisine: ["Street Food", "Beverages", "Tea"],
-averageCost: 70,
-isPro: true,
-isOff: 80,
-durationOfdelivery:47,
-restaurantReviewValue: 4.12,
-},
-   {
-_id: "123456-2",
-photos:["https://b.zmtcdn.com/data/pictures/chains/3/19604063/419eae0f17e3c72b7c492fb2160d1cca.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"],
-name: "Muzzaffarabad Biryani",
-cuisine: ["Biryani"],
-averageCost: 100,
-isPro: true,
-isOff: 80,
-durationOfdelivery:47,
-restaurantReviewValue: 4.1,
-},
-   {
-_id: "123456-3",
-photos:["https://b.zmtcdn.com/data/pictures/8/800228/9746cf43dfb4217bf76988aba7cd23c5.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"],
-cuisine: ["Fast Food",  "Chinese", "South Indian", "Beverages"],
-name: "Marksmen",
-averageCost: 430,
-isPro: true,
-isOff: 80,
-durationOfdelivery:47,
-restaurantReviewValue: 4.1,
-},
+const [restaurantList, setRestaurantList] = useState([]);
 
-])
+const reduxState = useSelector(
+  (globalStore) => globalStore.restaurant.restaurants
+);
+
+useEffect(() => {
+  reduxState.restaurants && setRestaurantList(reduxState.restaurants);
+}, [reduxState.restaurants]);
 
 
     return (
--- Client/src/Components/RestaurantCard.jsx
+++ Client/src/Components/RestaurantCard.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import {AiTwotoneStar} from "react-icons/ai";
+import { getImage } from "../Redux/Reducer/Image/Image.action";
 
 const RestaurantCard = (props) => {
+    const [image, setImage] = useState({
+        images: [],
+      });
+      const dispatch = useDispatch();
+    
+      useEffect(() => {
+        props.photos &&
+          dispatch(getImage(props.photos)).then((data) =>
+            setImage(data.payload.image)
+          );
+      }, [props.photos]);
+
+
     return (
-        <>
+        <Link to={`/restaurant/${props._id}`} className="w-full">
             <div className="bg-white p-4 w-full mb-4 rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/3">
                 <div className="w-full h-56 lg:h-64 relative">
                     <div className="absolute w-full bottom-4 flex items-end justify-between">
@@ -18,7 +34,7 @@ const RestaurantCard = (props) => {
                             )}
                     </div>
                     <img 
-                    src={props.photos.length && props.photos[0]}
+                    src={image.images.length && image.images[0].location}
                     alt="food"
                       className="w-full h-full rounded-2xl"
 />
@@ -36,7 +52,8 @@ const RestaurantCard = (props) => {
                     </div>
                 </div>
             </div>
-        </>
+            </Link>
+    
     );
 };
 
